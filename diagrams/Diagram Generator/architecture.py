a503c441b0696592f9ecabd3e913d418d8c5a28b
jason4king21 <jason4king21@gmail.com>
2025-05-24 19:14:01 -0500

    Diagram

--- diagrams/Diagram Generator/architecture.py
+++ diagrams/Diagram Generator/architecture.py
@@ -4,16 +4,22 @@ from diagrams.aws.compute import Lambda
 from diagrams.aws.analytics import KinesisDataFirehose
 from diagrams.aws.storage import S3
 from diagrams.saas.analytics import Snowflake
+from diagrams.programming.language import Python
+from diagrams.onprem.client import Client
 
 
 
-with Diagram("Real-Time Streaming Pipeline", show=False, filename="../diagrams/architecture", outformat="png"):
-    api = APIGateway("API Gateway")
-    lam = Lambda("Lambda Processor")
-    firehose = KinesisDataFirehose("Firehose")
+with Diagram("ETL - SCD1 using Snowflake Tasks, Streams & Stored Procedure", show=False, filename="diagrams/architecture", outformat="png"):
+    # api = APIGateway("API Gateway")
+    # lam = Lambda("Lambda Processor")
+    # firehose = KinesisDataFirehose("Firehose")
     s3 = S3("S3 Bucket")
-    s3error = S3("Error Logs")
-    snowflake = Snowflake("Snowflake")
+    # s3error = S3("Error Logs")
+    snowflake = Snowflake("Snowflake Table")
+    stream = Snowflake("Stream")
+    task = Snowflake("Task")
+    sp = Snowflake("StoredProc")
+    python = Python("Anaconda Python")
+    csv = Client("Local .csv file")
 
-    api >> lam >> firehose >> s3 >> snowflake
-    lam >> s3error
+    csv >> python >> s3 >> stream >> task >> sp >> snowflake
